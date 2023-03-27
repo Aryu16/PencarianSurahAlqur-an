8ec6934a2fad1a7322daf92d976a7b0c5776dae1
Aryu16 <45057161+Aryu16@users.noreply.github.com>
2023-03-27 09:52:05 +0700

    upload pertamax

---
+++ main.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form');
+const hasil = document.querySelector('#hasil');
+
+form.addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+  const surah = form.elements.surah.value;
+  const awal = form.elements.awal.value;
+  const akhir = form.elements.akhir.value;
+
+  const response = await fetch(`https://api.alquran.cloud/v1/ayah/${surah}:${awal}-${akhir}`);
+  const data = await response.json();
+
+  let html = '';
+  data.data.forEach(ayah => {
+    html += `<p>${ayah.text}</p>`;
+  });
+
+  hasil.innerHTML = html;
+});
